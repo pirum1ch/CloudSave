50f3e7d1fe1300bd2f811db2fa60a0daad732665
dmitriy.pirumov <dmitry.pirumov@gmail.com>
2024-10-07 14:46:03 +0300

    Controllers /files added

--- src/main/java/ru/pirum1ch/cloudsave/controllers/UploadController.java
+++ src/main/java/ru/pirum1ch/cloudsave/controllers/UploadController.java
@@ -1,10 +1,30 @@
 package ru.pirum1ch.cloudsave.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/file")
 public class UploadController {
 
+    @PostMapping
+    public String upload (String file){
+        return null;
+    }
+
+    @DeleteMapping
+    public String delite(String file){
+        return null;
+    }
+
+    @GetMapping
+    public List<String> getListOfAllFiles(){
+        return null;
+    }
+
+    @PutMapping
+    public String updateFile (String file){
+        return null;
+    }
 }
